68101e9f78e30f42d73a7b14985fcb080be1b0b0
Christopher Schmitt <schmitt@heatvision.com>
2013-07-30 22:25:32 -0500

    old site.

---
+++ inc/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html> <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]--><!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]--><!--[if IE 8]><html class="no-js lt-ie9"><![endif]--><!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+        <title>Artifact Conference 2013</title>
+    </head>
+    <body>
+
+        <div id="pagetop" class="main-container">
+            <div class="constellations">
+           
+                <div class="site_header">
+                    <div class="mobile_nav"><a class="nav_link" href="#footer_nav">Navigation</a></div>
+                    <h1 class="logo">
+                        <a <?php if($page == 'index'): ?>class="scroll_button"<?php endif; ?> data-nav="#pagetop" href="<?php echo $config['root']; ?>">HOME</a>
+                    </h1>
+                    <ul class="nav clearfix">
+                        <li><a <?php if($page == 'index'): ?>class="scroll_button"<?php endif; ?> data-nav="#speakers" href="<?php echo $config['root']; ?>#speakers">Speakers</a></li>
+                        <li><a <?php if($page == 'index'): ?>class="scroll_button"<?php endif; ?> data-nav="#program" href="<?php echo $config['root']; ?>#program">Program</a></li>
+                        <li><a <?php if($page == 'index'): ?>class="scroll_button"<?php endif; ?> data-nav="#workshops" href="<?php echo $config['root']; ?>#workshops">Workshops</a></li>
+                        <li><a <?php if($page == 'index'): ?>class="scroll_button"<?php endif; ?> data-nav="#location" href="<?php echo $config['root']; ?>#location">Location</a></li>
+                        <li><a <?php if($page == 'index'): ?>class="scroll_button"<?php endif; ?> data-nav="#register" href="#register">Register</a></li>
+                    </ul>
+
+                    <div class="social">
+                                <ul>
+                                    <li><a class="social-twitter" href="http://twitter.com/artifactconf">Twitter</a></li>
+                                <!-- <li><a class="social-facebook" href="#">Facebook</a></li> -->
+                                    <li><a class="social-lanyrd" href="http://lanyrd.com/2013/artifact-conference/">Lanyrd</a></li>
+                                </ul>
+                            </div>
+
+                </div> <!-- END .header -->
+
+                <div class="main clearfix">
+                    <div class="section hero">
+                        <h1 class="logo">
+                            <a href="<?php echo $config['root']; ?>"><img src="img/logo.png" alt="Artifact Conference" title="Artifact Conference"></a>
+                        </h1>
+                        <h2 class="section-header">May 13-15, 2013 • Austin, TX</h2>
+                    </div>
+
+                 
---
+++ sponsors.php
@@ -0,0 +1,32 @@
+<?php 
+    include('inc/config.php');
+    
+    $page = 'privacy';
+    include('inc/header.php'); 
+?>
+
+
+                   
+					
+					<div id="privacy" class="section generic-content">
+                      
+                            <h2 class="section-header">Sponsorships</h2>
+                            
+                            <div class="content">
+                               
+                               
+                              If you would like to sponsor the Artifact conference, please email <a href="mailto:info@unmatchedstyle.com" title="">info@unmatchedstyle.com</a>.
+
+                            </div>
+
+                           
+                   
+                    </div> 
+
+                </div>
+					
+                   
+
+<?php 
+    include('inc/footer.php'); 
+?>
